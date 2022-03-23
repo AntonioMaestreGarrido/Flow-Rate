7a15548495e54bd159661e9628987dec2c46ab72
AntonioMaestreGarrido <91852508+AntonioMaestreGarrido@users.noreply.github.com>
2022-03-23 03:55:49 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,108 @@
-console.log("test");
+
+
+let ATsAct, ATsMax, ATsMin, ATsOpt;
+let StowRateAct, StowRateMin, StowRateMax, StowRateOpt;
+let InductRateAct, InductRateMin, InductRateMax, InductRateOpt;
+let MinutesToCheck, ATsAtTime;
+let date = new Date();
+let encendido = false;
+
+const refreshButton = document.getElementById("refresh");
+const encenderButton = document.getElementById("encender");
+
+refreshButton.addEventListener("click", () => apitest());
+encenderButton.addEventListener("click", () => {
+  encendido = !encendido;
+  if (encendido) {
+    encenderButton.innerText = "On";
+    main();
+  } else {
+    encenderButton.innerText = "Of";
+  }
+});
 
 async function apitest() {
-    let data2
- await  fetch("http://localhost:3000")
+  let data2;
+  //await  fetch("http://localhost:3000")
+  /*
+ Dirección IPv4. . . . . . . . . . . . . . : 192.168.15.230
+ Máscara de subred . . . . . . . . . . . . : 255.255.255.0
+ Puerta de enlace predeterminada . . . . . : 192.168.15.1
+
+ stowRate, ATs, inductRate
+ */
+  await fetch("http://localhost:3000")
     .then((response) => response.json())
-    .then((data) =>{let datos=data;test(data)});
-    
- 
- 
+    .then((data) => {
+      let datos = data;
+      calculate(datos);
+    })
+    .catch(() => alert("No se encuentra el servidor"));
+}
+function calculate(data) {
+  let date = new Date();
+  ATsAct = data.ATs;
+  StowRateAct = data.stowRate;
+  InductRateAct = data.inductRate;
+  let stowRateMinute = StowRateAct / 60;
+  let inductRateMinute = InductRateAct / 60;
+  MinutesToCheck = 15 - (date.getMinutes() % 15);
+  ATsAtTime = parseInt(
+    ((InductRateAct - StowRateAct) / 60) * MinutesToCheck + ATsAct
+  );
+
+  ATsMax = StowRateAct / 2;
+  ATsMin = StowRateAct / 4;
+  ATsOpt = (ATsMax + ATsMin) / 2;
+
+  StowRateMin = parseInt((StowRateAct / ATsMin) * ATsAct);
+  StowRateMax = parseInt((StowRateAct / ATsMax) * ATsAct);
+  StowRateOpt = parseInt((StowRateAct / ATsOpt) * ATsAct);
+
+  InductRateMin = parseInt((InductRateAct / ATsMin) * ATsAct);
+  InductRateMax = parseInt((InductRateAct / ATsMax) * ATsAct);
+  InductRateOpt = parseInt((InductRateAct / ATsOpt) * ATsAct);
+
+  filltable();
 }
 
-async function test(data){
-    console.log('data')
-    console.log(data)
-    document.getElementById('ATs').innerText=`Tenemos un total de ${data.ATs} ATs`
-    document.getElementById('stowRate').innerText=`Tenemos un ritmo de Stow de ${data.stowRate} paquetes por hora`
-    document.getElementById('inductRate').innerText=`Tenemos un ritmo de induccion de ${data.inductRate} paquetes por hora`
+async function filltable() {
+  document.getElementById("ATsAct").innerText = ATsAct;
+  document.getElementById("ATsMax").innerText = ATsMax;
+  document.getElementById("ATsMin").innerText = ATsMin;
+  document.getElementById("ATsOpt").innerText = ATsOpt;
+
+  document.getElementById("StowRateAct").innerText = StowRateAct;
+  document.getElementById("StowRateMax").innerText = StowRateMax;
+  document.getElementById("StowRateMin").innerText = StowRateMin;
+  document.getElementById("StowRateOpt").innerText = StowRateOpt;
+
+  document.getElementById("InductRateAct").innerText = InductRateAct;
+  document.getElementById("InductRateMax").innerText = InductRateMax;
+  document.getElementById("InductRateMin").innerText = InductRateMin;
+  document.getElementById("InductRateOpt").innerText = InductRateOpt;
+
+  document.getElementById("MinutesToCheck").innerText = MinutesToCheck;
+  document.getElementById("ATsAtTime").innerText = ATsAtTime;
+  giveStyle();
 }
-apitest()
+function giveStyle() {
+  if (ATsAct > ATsMax || ATsAct < ATsMin) {
+    document.getElementById("ATsAct").style.backgroundColor = "red";
+  } else {
+    document.getElementById("ATsAct").style.backgroundColor = "green";
+  }
+  if (ATsAtTime > ATsMax || ATsAtTime < ATsMin) {
+    document.getElementById("ATsAtTime").style.backgroundColor = "red";
+  } else {
+    document.getElementById("ATsAtTime").style.backgroundColor = "green";
+  }
+}
+function main() {
+  do {
+    console.log("on");
+    console.log(encendido)
+    //apitest()
+  } while (encendido);
+}
+setInterval('apitest()',30000)
